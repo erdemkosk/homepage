d635845323e271300bb16f993e15e9b3b8b95578
Erdem Kosk <erdem.kosk@getir.com>
2021-07-23 19:45:45 +0300

    Chnaged google analitics logic

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/gtag'
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="tr">
         <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
           <script
             dangerouslySetInnerHTML={{
-              __html: `UA-46287596-1`,
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
             }}
           />
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
