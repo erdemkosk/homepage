7288a863ef6052f4bb4a9a86e800bab81dc9838c
Erdem Kosk <erdem.kosk@getir.com>
2021-07-23 20:04:16 +0300

    Added analitics for realtime users

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '@/css/tailwind.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
@@ -6,6 +9,17 @@ import Head from 'next/head'
 import LayoutWrapper from '@/components/LayoutWrapper'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ThemeProvider attribute="class">
       <Head>
